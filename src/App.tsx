150e20d254bf0428c0025bcbf52d032457ba894a
ruivergani <ruiverganineto@gmail.com>
2024-07-28 14:20:22 +0100

    Commit: React Helmet SEO

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import "./global.css"
 import { RouterProvider } from "react-router-dom"
 
 import { router } from "./routes"
+import { Helmet, HelmetProvider } from "react-helmet-async"
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Web Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom"
 
 import { AppLayout } from "./pages/_layouts/app"
+import { AuthLayout } from "./pages/_layouts/auth"
 import { Dashboard } from "./pages/app/dashboard"
 import { SignIn } from "./pages/auth/sign-in"
-import { AuthLayout } from "./pages/_layouts/auth"
 
 export const router = createBrowserRouter([
   {
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
   },
   {
     path: "/sign-in",
-    element: <AuthLayout/>,
+    element: <AuthLayout />,
     children: [{ path: "/sign-in", element: <SignIn /> }],
   },
 ])
