42de5bea9e806b95ebfefb24549707b81dcee19a
ruivergani <ruiverganineto@gmail.com>
2024-07-29 16:50:28 +0100

    Commit: ThemeToggle done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@ import { Home, Pizza, UtensilsCrossed } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
 
 import { NavLink } from "./nav-link"
+import { ThemeToggle } from "./theme/theme-toggle"
 
 export function Header() {
   return (
@@ -25,6 +26,10 @@ export function Header() {
             Orders
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
       </div>
     </div>
   )
