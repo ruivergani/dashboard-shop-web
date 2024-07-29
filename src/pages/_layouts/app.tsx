740208db66a1b5d8b822a8004314ed4b0ff5c031
ruivergani <ruiverganineto@gmail.com>
2024-07-29 16:23:44 +0100

    Commit: Register restaurant and Header Layout

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 
+import { Header } from "@/components/header"
+
 export function AppLayout() {
   return (
-    <>
-      <h1>Cabecalho</h1>
-      <div>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header></Header>
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
-    </>
+    </div>
   )
 }
---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,131 @@
+import { Label } from "@radix-ui/react-label"
+import { Helmet } from "react-helmet-async"
+import { useForm } from "react-hook-form"
+import { Link, useNavigate } from "react-router-dom"
+import { toast } from "sonner"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+const signUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+})
+type signUpForm = z.infer<typeof signUpForm>
+
+export function SignUp() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<signUpForm>()
+
+  async function handleSignUp(data: signUpForm) {
+    try {
+      console.log(data)
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      toast.success("Restaurant registered successfully.", {
+        action: {
+          label: "Login",
+          onClick: () => navigate("/sign-in"),
+        },
+      })
+    } catch {
+      toast.error(
+        "Error when registering you as our partner. Try again, unexpected error occured.",
+      )
+    }
+  }
+
+  return (
+    <>
+      <Helmet title="Register" />
+      <div className="p-8">
+        <Button
+          asChild
+          variant={"secondary"}
+          className="absolute right-8 top-8"
+        >
+          <Link to="/sign-in">Sign In</Link>
+        </Button>
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Create free account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Be our partner and start your sales!
+            </p>
+          </div>
+          <form
+            action=""
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(handleSignUp)}
+          >
+            <div className="space-y-2">
+              <Label htmlFor="restaurantName">Restaurant Name</Label>
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register("restaurantName")}
+              ></Input>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="managerName">Your Name</Label>
+              <Input
+                id="managerName"
+                type="text"
+                {...register("managerName")}
+              ></Input>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Your E-mail</Label>
+              <Input
+                id="email"
+                type="email"
+                {...register("email")}
+              ></Input>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="phone">Your Phone</Label>
+              <Input
+                id="phone"
+                type="tel"
+                {...register("phone")}
+              ></Input>
+            </div>
+            <Button
+              className="w-full"
+              disabled={isSubmitting}
+            >
+              Complete Registration
+            </Button>
+
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              By continuing, you agree to our{" "}
+              <a
+                href=""
+                className="underline underline-offset-4"
+              >
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a
+                href=""
+                className="underline underline-offset-4"
+              >
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
