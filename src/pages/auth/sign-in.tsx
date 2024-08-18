72df127aab4e285b0133062f145135bff3c1474f
ruivergani <ruiverganineto@gmail.com>
2024-08-18 18:00:22 +0100

    Commit: Register Restaurant API

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,7 @@
 import { useMutation } from "@tanstack/react-query"
 import { Helmet } from "react-helmet-async"
 import { useForm } from "react-hook-form"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import { toast } from "sonner"
 import { z } from "zod"
 
@@ -16,11 +16,17 @@ const signInForm = z.object({
 type signInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<signInForm>()
+  } = useForm<signInForm>({
+    defaultValues: {
+      email: searchParams.get("email") ?? "",
+    },
+  })
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: signIn,
@@ -29,7 +35,6 @@ export function SignIn() {
   async function handleSignIn(data: signInForm) {
     try {
       await authenticate({ email: data.email })
-      
       toast.success("Sent an authentication link to your e-mail.", {
         action: {
           label: "Send again",
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,10 +1,12 @@
 import { Label } from "@radix-ui/react-label"
+import { useMutation } from "@tanstack/react-query"
 import { Helmet } from "react-helmet-async"
 import { useForm } from "react-hook-form"
 import { Link, useNavigate } from "react-router-dom"
 import { toast } from "sonner"
 import { z } from "zod"
 
+import { registerRestaurant } from "@/api/register-restaurant"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
@@ -25,14 +27,21 @@ export function SignUp() {
     formState: { isSubmitting },
   } = useForm<signUpForm>()
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
   async function handleSignUp(data: signUpForm) {
     try {
-      console.log(data)
-      await new Promise(resolve => setTimeout(resolve, 2000))
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        phone: data.phone,
+        email: data.email,
+      })
       toast.success("Restaurant registered successfully.", {
         action: {
           label: "Login",
-          onClick: () => navigate("/sign-in"),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       })
     } catch {
